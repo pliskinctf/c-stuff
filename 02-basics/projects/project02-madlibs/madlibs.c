904a82f095542b346c62d155c63a6f941c782c95
pliskinctf <pliskinctf@protonmail.com>
2026-03-11 00:25:18 +0500

    Completed Project 2: Madlibs Game.

---
+++ 02-basics/projects/project02-madlibs/madlibs.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    char persons_name[50];
+    char adjective[50];
+    char object_name[50];
+    char verb[50];
+    char silly_word[50];
+    int no_of_times;
+
+    printf("enter a person's name: ");
+    scanf("%s", persons_name);
+
+    printf("enter an adjective: ");
+    scanf("%s", adjective);
+
+    printf("enter an object's name: ");
+    scanf("%s", object_name);
+
+    printf("enter a verb: ");
+    scanf("%s", verb);
+
+    printf("enter a silly word: ");
+    scanf("%s", silly_word);
+
+    printf("enter a random number: ");
+    scanf("%d", &no_of_times);
+
+    printf("yesterday, %s found a %s %s that could %s whenever someone said %s\nrandom number:\t %d\n", persons_name, adjective, object_name, verb, silly_word, no_of_times);
+
+    return 0;
+}
